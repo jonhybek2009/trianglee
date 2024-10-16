a7574f8d45d4837499c70ccf114864eb2b7d9902
Amin_stors <amangulovzyrgalbek@gmail.com>
2024-10-16 16:34:38 +0600

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        triangle triangle = new triangle(3,4,5);
+        Triangle triangle = new Triangle(3, 4, 5);
         System.out.println("Площадь треугольника: " + triangle.area());
     }
 }
--- src/triangle.java
+++ src/triangle.java
@@ -1,14 +1,14 @@
-public class triangle {
+class Triangle {
     double a, b, c;
 
-    triangle(double a, double b, double c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
+    Triangle(double Aside, double Bside, double Cside) {
+        a = Aside;
+        b = Bside;
+        c = Cside;
     }
-
+    // Метод для вычисления площади по формуле Герона
     double area() {
-        double p = (a + b + c) / 2;
-        return Math.sqrt(p * (p - a) * (p - b) * (p-c));
-
-}}
+        double p = (a + b + c) / 2;  // Полупериметр
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+}
